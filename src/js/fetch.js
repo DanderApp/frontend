e1ac64500937c4db613b41fdbbd467d0d12ca0a5
Zachary Smith <billyzacsmith@gmail.com>
2016-01-10 12:00:29 -0700

    Fetch some fake puppy data and console.log it.

---
+++ src/js/fetch.js
@@ -0,0 +1,13 @@
+var xhr = require('xhr')
+
+module.exports = function(uri) {
+  return new Promise(function(resolve, reject) {
+    xhr(uri, function(err, res, body) {
+      if (err)
+        return reject(err)
+      if (res.statusCode != 200)
+        return reject(new Error(body))
+      resolve(body)
+    })
+  })
+}
--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,10 @@
 var liking = require('./liking')
 var initialize = require('./initialize')
 var $ = require('jquery')
+var fetch = require('./fetch')
+
+var puppyURL = __dirname + 'fake-data.json'
+var getPuppies = fetch(puppyURL)
 
 // Invoke them here.
 // We'll probably have things like
@@ -11,6 +15,13 @@ var $ = require('jquery')
 
 // We won't have very much code here, just invocations of functions declared elsewhere.
 $(function() {
+  getPuppies
+    .then(function(pets) {
+      console.log(pets)
+    })
+    .catch(function(err) {
+      console.error('There was an error fetching the pets', err)
+    })
   initialize.hideShelterInfo()
   liking.handleLike()
   liking.handleDisLike()
