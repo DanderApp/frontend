2db9bd3fa5fe71008b1ab7abc3d10c30955e13bf
Zachary Smith <billyzacsmith@gmail.com>
2016-01-09 21:16:17 -0700

    Set up basic gulp static site pipeline. To rebuild the public folder, run `gulp`. This shuttles everything in 'src' to 'public'.
    Also initialized with npm.

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp')
+
+gulp.task('default', ['build'])
+
+gulp.task('build',
+  [
+    'build:html',
+    'build:js',
+    'build:images',
+    'build:css'
+  ])
+
+gulp.task('build:html', function() {
+  return gulp
+    .src('./src/**/*.html')
+    .pipe(gulp.dest('./public'))
+})
+
+gulp.task('build:js', function() {
+  return gulp
+    .src('./src/js/main.js')
+    .pipe(gulp.dest('./public'))
+})
+
+gulp.task('build:images', function() {
+  return gulp
+    .src('./src/images/*')
+    .pipe(gulp.dest('./public/images'))
+})
+
+gulp.task('build:css', function() {
+  return gulp
+    .src('./src/**/*.css')
+    .pipe(gulp.dest('./public'))
+})
