f5d393e5e2abddabee7cfc4fed9af4ff0cd5ad0f
Zachary Smith <billyzacsmith@gmail.com>
2016-01-13 07:25:17 -0700

    When user clicks the login button (on the fake login page) make a post request to a (fake) server. Then set a token that stores the user data.

--- src/js/auth.js
+++ src/js/auth.js
@@ -2,18 +2,30 @@ var $ = require('jquery')
 
 function handleLogin() {
   $('#login').click(function(event) {
-
+    var userCredentials = {
+      name: 'Zeb',
+      password: 'letmein'
+    }
     $.ajax(
       {
-        url: 'http://localhost:3000/auth/facebook',
-        method: 'get'
+        url: 'https://our.server/authenticate',
+        method: 'post',
+        data: userCredentials
       }
-    ).done(function(data) {
-      console.log('Success. Here\'s the data: ' + data)
+    ).done(function(response) {
+      console.log('Success. Here\'s the response: ' + response)
+    }).fail(function(msg) {
+      console.log('Failed to log in ' + msg)
+    }).always(function(data) {
+      console.log('Set the user session token, whether or not you successfully logged in. Open up Resources > Session Storage.')
+      console.log('In the real world, we\'ll only do this upon successful authentication, of course, but we\'re faking it until the server-side auth is up and running.')
+      // Store the user on the session.
+      sessionStorage.setItem('token', JSON.stringify(userCredentials))
     })
   })
 }
 
 $(function() {
+  console.log('See this auth stuffz?')
   handleLogin()
 })
