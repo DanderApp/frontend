a348be938c3254516da87f72ed6a760d6eeba33a
Zachary Smith <billyzacsmith@gmail.com>
2016-01-13 10:27:30 -0700

    Use our api instead of the local fake data.

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,8 @@ var initialize = require('./initialize')
 var $ = require('jquery')
 var fetch = require('./fetch')
 
-var puppyURL = __dirname + 'fake-data-2.json'
+// var puppyURL = __dirname + 'fake-data-2.json'
+var puppyURL = 'http://dander.herokuapp.com/apitest'
 var getPuppies = fetch(puppyURL)
 
 // We won't have very much code here, just invocations of functions declared elsewhere.
