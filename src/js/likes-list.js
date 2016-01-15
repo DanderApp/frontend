e4290b25890283df62a9331f3ef6081b0b04cf38
Zachary Smith <billyzacsmith@gmail.com>
2016-01-14 18:55:54 -0700

    Fix bug with likes list. Previously, list was showing the same dog, over and over.

--- src/js/likes-list.js
+++ src/js/likes-list.js
@@ -2,18 +2,25 @@ var $ = require('jquery')
 var display = require('./display')
 var petIndex = require('./counter')
 
+function getUser() {
+  if(localStorage.token) {
+    return JSON.parse(atob(localStorage.token.split('.')[1])).user;
+  } else {
+    return null
+  }
+}
+
 function handleDisplayLikes() {
+  var user = getUser()
   $('.likes-list-header').click(function() {
     $.ajax({
-      url: 'https://dander.herokuapp.com/connections',
-      method: 'get',
+      url: 'https://dander.herokuapp.com/connections/?id=' + user.id,
+      method: 'get'
     })
       .then(function(connectionsData) {
         console.log(connectionsData)
-
         // Clear the likes list
         $('#fixed-tab-2').empty()
-
         // For every pet in the list, create a card and add them to the page
         var length = connectionsData.length
         for (var i=0; i<length; i++) {
@@ -22,14 +29,12 @@ function handleDisplayLikes() {
           var blankCard =
             '<div class="puppy-card mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-col">' +
               '<div class="mdl-card__media">' +
-                '<img class= "profile-photo" src="./images/puppy-01.jpeg" width="100%" alt="" style="padding:0px;"/>' +
+                '<img class= "profile-photo-' + i + '" src="" width="100%" alt="" style="padding:0px;"/>' +
               '</div>' +
               '<div class="mdl-card__title">' +
-                '<h2 class="name mdl-card__title-text">' +
-                '</h2>' +
+                '<h2 class="name-' + i + '" mdl-card__title-text"></h2>' +
               '</div>' +
-              '<div class="pet-info mdl-card__supporting-text">' +
-              '</div>' +
+              '<div class="pet-info-' + i + '" mdl-card__supporting-text"></div>' +
               '<div class="mdl-card__actions mdl-card--border">' +
                 '<a id="" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="#">Contact shelter</a>' +
               '</div>' +
@@ -37,7 +42,7 @@ function handleDisplayLikes() {
           $('#fixed-tab-2').append(blankCard)
 
           // Fill out the card with the pet's info
-          $('.name').text(pet.name)
+          $('.name-'+i).text(pet.name)
           if (pet.sex === 'M')
             pet.sex = 'Male'
           if (pet.sex === 'F')
@@ -49,9 +54,9 @@ function handleDisplayLikes() {
           if (pet.size === 'L')
             pet.size = 'Large'
           var infoText = pet.sex + ' | ' + pet.age + ' | ' + pet.size
-          $('.pet-info').text(infoText)
+          $('.pet-info-'+i).text(infoText)
           pet.photo = pet.photo
-          $('.profile-photo').attr('src', pet.photo)
+          $('.profile-photo-'+i).attr('src', pet.photo)
         }
       })
       .fail(function() {
